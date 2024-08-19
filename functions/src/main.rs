92847b6ad5cc80ed8c5952a8e1fb06bdecdd489e
Anand Prabhu <anandprabhu530@gmail.com>
2024-08-19 22:38:03 +0530

    Functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     let a: i32 = 10;
     let c = new_function(a);
     println!("{c}");
+    let (d ,test) = sting_returner();
+    println!("{d} {test}"); 
 }
 
 //snake case is preffered for function names
@@ -9,7 +11,12 @@ fn main() {
 fn new_function(a:i32)->i32{
     println!("Inside New_function");
     let d = a * 4;
-
     //will be returned default by mentioning the variable only ---Without Semicolon---
     d
+}
+
+//multiple return 
+fn sting_returner() -> (String,i32){
+    let a = String::from("hello from string returner");
+    return (a , 8);
 }
