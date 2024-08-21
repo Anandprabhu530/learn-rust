9441f79b60aef1bf7cc41437d1eb4ecbb00295ce
Anand Prabhu <anandprabhu530@gmail.com>
2024-08-21 22:11:13 +0530

    Mutable params

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -4,6 +4,10 @@ fn main() {
     println!("{c}");
     let (d ,test) = sting_returner();
     println!("{d} {test}"); 
+    let ans = String::from("Hello");
+    let ans1 = mutatble_ans(ans);
+    println!("{}",ans1);
+
 }
 
 //snake case is preffered for function names
@@ -19,4 +23,10 @@ fn new_function(a:i32)->i32{
 fn sting_returner() -> (String,i32){
     let a = String::from("hello from string returner");
     return (a , 8);
+}
+
+
+fn mutatble_ans(mut test1:String)->String{
+    test1.push_str(" pushed here");
+    test1
 }
