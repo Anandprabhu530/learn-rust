7d40ca09aeacf27363f1a2be2771c7378c442486
Athesh <106463254+Athesh007@users.noreply.github.com>
2024-08-25 13:12:59 +0530

    Reset Done

---
+++ structs/src/main.rs
@@ -0,0 +1,27 @@
+struct Laptop{
+    company:String,
+    ram:i32,
+    hdd:i32,
+    os:Option<String>
+}
+
+fn main() {
+    let mut a = Laptop{
+        company:String::from("Dell"),
+        ram:16,
+        hdd:1,
+        os:Some(String::from("Linux"))
+    };
+    println!("Laptop Brand:{}  Ram:{}gb, HDD:{}TB, OS:{:?}",a.company,a.ram,a.hdd,a.os);
+
+    println!("Upgrade Laptop");
+    a.ram = 32;
+    a.hdd = 2;
+    if let Some(os) = &a.os {
+        println!("{:?}'s bag has {}!", a.os, os)
+    } else {
+        println!("{}'s bag is empty!", a.company)
+    }
+    println!("Laptop Brand:{}  Ram:{}gb, HDD:{}TB",a.company,a.ram,a.hdd);
+
+}
