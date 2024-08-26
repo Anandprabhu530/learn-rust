49c4b2f9e2f1d0969ec923ef80baad0c83a360e7
Anandprabhu530 <anandprabhu530@gmail.com>
2024-08-26 20:04:58 +0530

    Two problems added

--- Stringproblems/src/main.rs
+++ Stringproblems/src/main.rs
@@ -1,9 +1,44 @@
 fn main() {
-    let ans = interpret(String::from("(al)G(al)()()G"));
+    let str1 = String::from("LLLLRRRR");
+    let ans = balanced_string_split(str1);
     println!("{}",ans);
+  }
+
+//https://leetcode.com/problems/truncate-sentence/description/
+fn _truncate_sentence(s: String, k: i32) -> String {
+    let mut a = 0;
+    let mut ans = String::new();
+    for words in s.split_whitespace(){
+        ans.push_str(&String::from(words));
+        a+=1;
+        if a>=k{
+            return ans;
+        }
+        ans.push_str(&String::from(" "));
+    }
+    ans
 }
 
-fn interpret(command: String) -> String {
+//https://leetcode.com/problems/split-a-string-in-balanced-strings/description/
+fn balanced_string_split(s: String) -> i32 {
+    let mut l =0;
+    let mut ans =0;
+    for word in s.chars(){
+        if word=='L'{
+            l+=1;
+        }else{
+            l-=1;
+        }
+        if l==0{
+            ans+=1;
+        }
+    }
+    
+    ans
+}
+
+//G()(a)l --> Leetcode
+fn _interpret(command: String) -> String {
     let mut ans = String::from("");
     let mut check = false;
 
