ce48471934f667cf370a8c99b0347936dd40c689
Anandprabhu530 <anandprabhu530@gmail.com>
2024-08-30 22:04:28 +0530

    Basic match Pattern

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -32,6 +32,41 @@ fn main(){
     let movie2 = Category::Horror(String::from("Friday 13th"));
     let movie3 = Category::Adventure(String::from("Tomb Raider"));
 
-
     println!("Movie 1: {:#?}, Movie 2: {:?}, Movie 3: {:?}",movie1,movie2,movie3);
+
+    let sample1 = Some(String::from("Hello World"));
+    let sample2 = Some(32);
+    let temp = 2;
+    let sample3:Option<String> = None;
+    
+    //the below is not possible bcz i32 and Option<i32> are different types
+    //we use match for it
+    // let sum = sample2+temp;
+
+    //match the pattern
+    fn add_number(x:Option<i32>,)->i32{
+        match x{
+            None=>0,
+            Some(a) => a,
+        }
+    }
+
+    let ans = add_number(sample2);
+
+    //Can also return the option with some itself
+    fn add_number2(x:Option<i32>,b:i32)->Option<i32>{
+        match x{
+            None=>None,
+            Some(a) => Some(a+b),
+        }
+    }
+
+    let ans2 = add_number2(sample2,temp);
+    println!("The ans is {:?}", ans2);
+
+    let sum = ans + temp;
+    println!("The ans is {:?}", sum);
+    
+   
 }
+
