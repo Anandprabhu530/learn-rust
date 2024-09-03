f35666a55b98f2de877f11edca4a02230ab81e6b
Anandprabhu530 <anandprabhu530@gmail.com>
2024-09-03 22:27:25 +0530

    CLI app

--- cli-app/src/main.rs
+++ cli-app/src/main.rs
@@ -1,11 +1,29 @@
-use std::io;
+use std::io::{self, Write};
 
 fn main() {
+    print!("Please Enter your username: ");
     let mut username = String::new();
+    io::stdout().flush().unwrap();
+
     io::stdin()
         .read_line(&mut username)
         .expect("failed to read from stdin");
     let trimmed_username = username.trim();
-    println!("Welcome {}!! Let's Gamble",trimmed_username)
+
+    print!("Please Enter your password: ");
+    let mut password = String::new();
+    io::stdout().flush().unwrap();
+    io::stdin()
+        .read_line(&mut password)
+        .expect("Failed to read");
+    println!();
+
+    let trimmed_password = password.trim();
+    if trimmed_password.eq("admin") && trimmed_username.eq("admin") {
+        println!("Welcome {trimmed_username}!");
+    }else{
+        println!("welcome {trimmed_username}");
+    }
+
 }
 
